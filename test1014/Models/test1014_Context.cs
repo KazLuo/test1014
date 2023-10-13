51ffb56f42c8607f0106f3d3494469eff280582a
Kaz <ccc5211@gmail.com>
2023-10-14 01:13:59 +0800

    加入專案檔案。

---
+++ test1014/Models/test1014_Context.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace test1014.Models
+{
+    public partial class test1014_Context : DbContext
+    {
+        public test1014_Context()
+            : base("name=test1014")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
